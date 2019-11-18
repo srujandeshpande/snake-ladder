b8248929575bbb1ef2ab5174d308c8ebc9fd9712
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-11-18 08:59:07 +0530

    Added random number gen using curr time as seed

--- driver.c
+++ driver.c
@@ -2,11 +2,13 @@
 
 List* multilist[40];
 
-void createMultiList() 
+void roll();
+
+int createMultiList() 
 {
 	for(int i=0;i<40;i++) 
 	{
-		if(!multilist[i] = (List*)malloc(sizeof(List))) return 1);
+		multilist[i] = (List*)malloc(sizeof(List));
 		multilist[i]->visited = 0;
 		multilist[i]->node1 = 0;
 		multilist[i]->node2 = 0;
@@ -17,12 +19,20 @@ void createMultiList()
 
 void populateMultiList()
 {
-
-
+	(multilist[1])->node1 = 1;
+	multilist[1]->node2 = 2;
+	multilist[1]->link2 = multilist[2];
+	multilist[2]->node1 = 2;
+	multilist[2]->node2 = 3;
+	multilist[2]->link1 = multilist[3];
+	multilist[2]->link2 = multilist[4];
+	//TODO...
 }
 
 int main() 
 {
-	if(createMultiList()) return 1;
-	populateMultiList();
+	//if(createMultiList()) return 1;
+	//populateMultiList();
+	roll();
+	
 }
--- utility.c
+++ utility.c
@@ -1,4 +1,19 @@
 #include "header.h"
 
-//sdfsd
+void displayBoard()
+{
+	
+	
+}
 
+void roll()
+{
+	printf("Press r to roll\n");
+	time_t tt;
+	time(&tt);
+	int tnum = (int)tt;
+	srand(tnum);
+	int rnum = rand();
+	int num = rnum%6 +1;
+	printf("%d\n",num);
+}
